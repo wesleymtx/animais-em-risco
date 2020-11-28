f2f02af41f0d3e575978b7885083cbfccd1ffdca
unknown <you@example.com>
2020-11-28 16:33:12 -0300

    Mudanças de estilo no card

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import '../styles/CardItem.css'
 function CardItem(props) {
+  const [imageNotFound, setImageNotFound] = useState(false)
+  const fallBackImage = (e, n)=>{
+    setImageNotFound(true)
+    e.target.remove()
+  }
 return (
   <div className="card-item-container">
     <div className="pane-card">
-      <div className="photo-card">
-        <img src={props.imagem}></img>
+      <div className="photo-card-shape">
+        <img src={props.imagem} onError={(e)=>fallBackImage(e, props.nome)}></img>
+        {imageNotFound && <div style={{'width': '200%'}}>ok</div>}
       </div>
       <div className="content-card">
         <h1>{props.nome}</h1>
